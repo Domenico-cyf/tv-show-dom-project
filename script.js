9955e8a5120fc4d4d4176da899124f4dbea7cdbd
domenico.cyf <domenico.cyf@gmail.com>
2021-03-21 09:30:44 +0000

    First commitment

--- script.js
+++ script.js
@@ -7,6 +7,39 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
+  for (let episode in episodeList) {
+    console.log(episodeList[episode].name);
+    let episodeNum = episodeList[episode].number;
+    let seasonNum = episodeList[episode].season;
+
+    if (episodeNum < 10) {
+      episodeNum = `0` + episodeNum.toString();
+    }
+    if (seasonNum < 10) {
+      seasonNum = `0` + seasonNum.toString();
+    }
+    let seasonEpi = `S${seasonNum}E${episodeNum}`;
+    console.log(seasonEpi);
+
+    const title = document.createElement("div");
+    const displayList = document.getElementById("displayList");
+    console.log(displayList);
+    title.textContent = episodeList[episode].name + " - "+ seasonEpi;
+    displayList.appendChild(title); 
+
+    
+
+    const image = document.createElement("img");
+    console.log(image);
+    image.src = episodeList[episode].image.medium;
+    displayList.appendChild(image);
+
+    //console.log(summary);
+
+    displayList.innerHTML += episodeList[episode].summary;
+
+    
+  }
+}
 window.onload = setup;
