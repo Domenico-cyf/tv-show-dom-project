49459c45ef286339c69e326fadcf93c6fddc4e1c
domenico.cyf <domenico.cyf@gmail.com>
2021-03-31 19:44:39 +0100

    Added search bar level 200

--- script.js
+++ script.js
@@ -1,73 +1,94 @@
 //You can edit ALL of the code here
 function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
 
-const allEpisodes = getAllEpisodes();
-makePageForEpisodes(allEpisodes);
+  const input = document.getElementById("input");
+  let searchValue = "";
+  input.addEventListener("input", (event) => {
+    searchValue = event.target.value.toLowerCase();
 
+    //let episodeList = getAllEpisodes();
 
+    let filteredEpisodes = allEpisodes.filter((episode) => {
+      if (
+        episode.name.toLowerCase().includes(searchValue) ||
+        episode.summary.toLowerCase().includes(searchValue)
+      ) {
+        return episode;
+      }
+    });
+    
+    let idArr = filteredEpisodes.map((element) => element.id);
+    console.log(idArr);
 
-  let epiFilter;
-  
-  const input = document.getElementById('input')
-    input.addEventListener("input",(event) => {
-     
+    let idArrNoShow = allEpisodes.filter((element) => {
+      return !idArr.includes(element.id);
+    });
 
-       let searchValue = event.target.value.toLowerCase();
+    idArrNoShow = idArrNoShow.map((element) => element.id);
+    console.log(idArrNoShow);
 
-       let episodeList = getAllEpisodes();
+    for (let episode in idArrNoShow) {
+      let singleId = document.getElementById(idArrNoShow[episode]);
+      singleId.className = "hide";
+    }
+    for (let episode in idArr) {
+      let singleId = document.getElementById(idArr[episode]);
+      singleId.className = "show";
+    }
+    let root = document.getElementById("root");
+    root.textContent = `Got ${filteredEpisodes.length} episode(s)`;
+    //makePageForEpisodes(filteredEpisodes);
+  });
 
-       let filteredEpisodes = episodeList.filter((episode) => {
-         if (
-           episode.name.toLowerCase().includes(searchValue) ||
-           episode.summary.toLowerCase().includes(searchValue)
-         ) {
-           return episode;
-         }
-       });
-       showFiltered(filteredEpisodes);
-     //  epiFilter = makePageForEpisodes(filteredEpisodes);
-       console.log(filteredEpisodes);
-    })
-  
-  
-}
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${filteredEpisodes.length} episode(s)`;
+  // let episodeData = episodeList;
+  // if (searchValue) {
+  //   episodeData = filteredEpisodes;
+  // }
+  // const input = document.getElementById("input");
+  // input.addEventListener("input", (event) => {
+  //   let searchValue = event.target.value.toLowerCase();
 
-let showFiltered = function(filteredEpisodes){
-  console.log(filteredEpisodes)
+  //   //let episodeList = getAllEpisodes();
+
+  //   let filteredEpisodes = allEpisodes.filter((episode) => {
+  //     if (
+  //       episode.name.toLowerCase().includes(searchValue) ||
+  //       episode.summary.toLowerCase().includes(searchValue)
+  //     ) {
+  //       return episode;
+  //     }
+  //   });
+
+  //   console.log(filteredEpisodes);
+  //   makePageForEpisodes(filteredEpisodes);
+
+  // });
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
-
   for (let episode in episodeList) {
-    
     let episodeNum = episodeList[episode].number;
     let seasonNum = episodeList[episode].season;
 
-    if (episodeNum < 10) {
-      episodeNum = `0` + episodeNum.toString();
-    }
-    if (seasonNum < 10) {
-      seasonNum = `0` + seasonNum.toString();
-    }
-    let seasonEpi = `S${seasonNum}E${episodeNum}`;
-    
-    
+    let seasonEpi = episodeFun(episodeNum, seasonNum);
     const title = document.createElement("div");
-    
-   
+
     title.textContent = episodeList[episode].name + " - " + seasonEpi;
     //displayList.appendChild(title);
 
     const image = document.createElement("img");
-    
+
     image.src = episodeList[episode].image.medium;
     //displayList.appendChild(image);
 
-    
-   // displayList.innerHTML += episodeList[episode].summary;
+    // displayList.innerHTML += episodeList[episode].summary;
 
     const container = document.createElement("div");
     container.id = episodeList[episode].id;
@@ -75,8 +96,15 @@ function makePageForEpisodes(episodeList) {
     container.innerHTML += episodeList[episode].summary;
     displayList.appendChild(container);
   }
-
-
+  function episodeFun(episodeNum, seasonNum) {
+    if (episodeNum < 10) {
+      episodeNum = `0` + episodeNum.toString();
+    }
+    if (seasonNum < 10) {
+      seasonNum = `0` + seasonNum.toString();
+    }
+    return `S${seasonNum}E${episodeNum}`;
+  }
 }
 const foot = document.getElementById("foot");
 const link = document.createElement("a");
@@ -84,7 +112,5 @@ link.innerHTML = "Licencing";
 link.href = "https://www.tvmaze.com/api#licensing";
 link.title = "https://www.tvmaze.com/api#licensing";
 foot.appendChild(link);
-
-
 
 window.onload = setup;
